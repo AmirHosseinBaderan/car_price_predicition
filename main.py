1446054114d0a9aba623d38165569f6f158f4d2d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-08 22:55:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import pandas as pd
+from car_price_predicator import CarPricePredicator
 
-df = pd.load_excel('./cars_data.xls')
-
-print(df.head())
+def main():
+    predicator = CarPricePredicator(
+        excel_file="./cars_data.xls"
+    )
+    
+    predicator.fit()
+    predicator.evaluate()
+    
+    print("\n Price Prediction \n")
+    
+    car = {
+        "Mileage": 120000,
+        "Make": "Toyota",
+        "Model": "Corolla",
+        "Trim": "LE",
+        "Type": "Sedan",
+        "Cylinder": 4,
+        "Liter": 1.8
+    }
+    
+    predicted_price = predicator.predict(car)
+    print(
+        f"Predicted price : ${predicted_price:,.2f}"
+    )
+    
+if __name__ == "__main__":
+    main()
